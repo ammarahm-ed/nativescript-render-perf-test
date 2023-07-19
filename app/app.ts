4d90a6840031a4f096a2ff321df4e9c4cfe0ca43
ammarahm-ed <ammarahmed6506@gmail.com>
2023-07-19 22:12:35 +0500

    initial commit

--- app/app.ts
+++ app/app.ts
@@ -4,9 +4,73 @@ You can use this file to perform app-level initialization, but the primary
 purpose of the file is to pass control to the app’s first module.
 */
 
-import { Application } from '@nativescript/core'
+import {
+  Application,
+  FlexboxLayout,
+  ScrollView,
+  StackLayout,
+} from "@nativescript/core";
 
-Application.run({ moduleName: 'app-root' })
+function benchmark(
+  root: any,
+  Container: any,
+  containerProps: any,
+  Child: any,
+  childProps: any
+) {
+  return new Promise((resolve, reject) => {
+    const container = new Container();
+    root.content = container;
+    //@ts-ignore
+    for (const prop in containerProps) {
+      container[prop] = containerProps[prop];
+    }
+    setTimeout(() => {
+      console.log("Render test began");
+      console.time("Render test result");
+      for (let i = 0; i < 1000; i++) {
+        const view = new Child();
+        for (const prop in childProps) {
+          view[prop] = childProps[prop];
+        }
+        container.addChild(view);
+      }
+      console.timeEnd("Render test result");
+      console.log("Render test end");
+      resolve(true);
+    }, 1000);
+  });
+}
+
+Application.run({
+  create() {
+    const root = new ScrollView();
+    //@ts-ignore
+    root.width = "100%";
+    //@ts-ignore
+    root.height = "100%";
+
+    benchmark(
+      root,
+      FlexboxLayout,
+      {
+        width: "100%",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        justifyContent: "center",
+      },
+      StackLayout,
+      {
+        width: 100,
+        height: 100,
+        marginBottom: 10,
+        backgroundColor: "red",
+      }
+    );
+
+    return root;
+  },
+});
 
 /*
 Do not place any code after the application has been started as it will not
